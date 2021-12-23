2732722ec6c9b3e55ced105d8f29d0d593f823d5
H-Yuuki <yuukirinnma@gmail.com>
2021-12-23 20:04:26 +0900

    left・center・right.html ファイルを表示

--- flask_sample.py
+++ flask_sample.py
@@ -5,12 +5,12 @@ import ast
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
-@app.route('/html')
+@app.route('/')
 def index():
     return app.send_static_file('html/index.html')
     #return render_template("index.html")
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 80))
+    port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
 
