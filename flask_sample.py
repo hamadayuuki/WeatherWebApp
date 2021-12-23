0dbe531334c6ed41b29da1f5a35f6ea5902d8ef8
H-Yuuki <yuukirinnma@gmail.com>
2021-12-23 18:51:38 +0900

    Herokuにデプロイするためにファイルを修正2

--- flask_sample.py
+++ flask_sample.py
@@ -7,10 +7,10 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/html')
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('html/index.html')
     #return render_template("index.html")
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 80))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
